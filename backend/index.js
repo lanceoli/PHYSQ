2ed2c3d73a90a9960ae70f49dc35bb467f654050
andyrews <126462930+andyrews@users.noreply.github.com>
2025-03-01 17:45:19 +0800

    Added Database Connection

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log("tite")
+const express = require("express")
+const app = express()
+const port = 3000 
+const mongoose = require("mongoose")
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+mongoose.connect("mongodb+srv://johnandrewgacho:purgingorganics@physqcluster.hrhed.mongodb.net/Node-API?retryWrites=true&w=majority&appName=PHYSQCluster")
+.then(() => {
+    console.log("Connected to DB");
+    app.listen(port, () => {
+        console.log(`Server running on ${port}`)
+    })
+})
+.catch(() => {
+    console.log("Connection Failed!")
+})
