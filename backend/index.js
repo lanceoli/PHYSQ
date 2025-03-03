fed444be17db7365fedf0be47d9098d56c9cf2a1
andyrews <126462930+andyrews@users.noreply.github.com>
2025-03-03 18:15:38 +0800

    Added all model templates

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,10 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
-app.post("/api/signin", (req, res) => {
+app.post("/api/signin", async (req, res) => {
     try {
-        
+       const user = await User.create(req.body)
+       res.status(200).json(user)
     } catch (error) {
         res.status(500).json({message: error.message})
     }
