3a16e1b677ec9f7c8e9a9644958b5c25dfcce13a
andyrews <126462930+andyrews@users.noreply.github.com>
2025-03-06 03:48:45 +0800

    Added System Instructions for Gemini

--- backend/Gemini/apitest.js
+++ backend/Gemini/apitest.js
@@ -1,13 +1,16 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 async function call(prompt) {
-    const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        
-    const result = await model.generateContent(prompt);
-    return result.response.text();
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstruction: "You are a workout expert willing to give advice. Give most of the answers in table format and include some text",
+  });
+
+  const result = await model.generateContent(prompt);
+  return result.response.text();
 }
 
-module.exports = { call }
+module.exports = { call };
