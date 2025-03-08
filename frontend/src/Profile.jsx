d68f0c650509fefce6ff0ed3d688ded79d858265
lanceoli <lancedoli170@gmail.com>
2025-03-08 21:26:29 +0800

    fixed logout bug

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,12 +1,19 @@
 import authFunc from "./assets/isAuth"
+import { Link } from "react-router-dom"
 
 export default function Profile() {
     return (
     <>
     <div className="p-6 text-center">Profile</div>
 
+    <Link
+          className="btn btn-circle bg-white text-black border-none hover:bg-gray-200"
+          // to={ authFunc.auth ? "/Profile" : "/LogIn" }
+          to='/'
+        >
     <button onClick={authFunc.auth = false}> logout </button>
-    
+    </Link>
+
     </>
     )
   }
